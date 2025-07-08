487505a2953d1057a83c8d35ad633cbe41815724
weidixian <weidixian@126.com>
2025-07-08 20:18:02 +0800

    add codes for GoodsInfo Module.

---
+++ src/api/goods/info/index.ts
@@ -0,0 +1,63 @@
+import request from '@/utils/request';
+import { AxiosPromise } from 'axios';
+import { InfoVO, InfoForm, InfoQuery } from '@/api/goods/info/types';
+
+/**
+ * 查询商品信息列表
+ * @param query
+ * @returns {*}
+ */
+
+export const listInfo = (query?: InfoQuery): AxiosPromise<InfoVO[]> => {
+  return request({
+    url: '/goods/info/list',
+    method: 'get',
+    params: query
+  });
+};
+
+/**
+ * 查询商品信息详细
+ * @param id
+ */
+export const getInfo = (id: string | number): AxiosPromise<InfoVO> => {
+  return request({
+    url: '/goods/info/' + id,
+    method: 'get'
+  });
+};
+
+/**
+ * 新增商品信息
+ * @param data
+ */
+export const addInfo = (data: InfoForm) => {
+  return request({
+    url: '/goods/info',
+    method: 'post',
+    data: data
+  });
+};
+
+/**
+ * 修改商品信息
+ * @param data
+ */
+export const updateInfo = (data: InfoForm) => {
+  return request({
+    url: '/goods/info',
+    method: 'put',
+    data: data
+  });
+};
+
+/**
+ * 删除商品信息
+ * @param id
+ */
+export const delInfo = (id: string | number | Array<string | number>) => {
+  return request({
+    url: '/goods/info/' + id,
+    method: 'delete'
+  });
+};
---
+++ src/api/goods/info/types.ts
@@ -0,0 +1,264 @@
+export interface InfoVO {
+  /**
+   * ID
+   */
+  id: string | number;
+
+  /**
+   * 创建时间
+   */
+  createTime: string;
+
+  /**
+   * 乐观锁
+   */
+  version: number;
+
+  /**
+   * 店铺ID
+   */
+  shopId: string | number;
+
+  /**
+   * 分类ID
+   */
+  categoryId: string | number;
+
+  /**
+   * 编码
+   */
+  spuCode: string;
+
+  /**
+   * 名称
+   */
+  name: string;
+
+  /**
+   * 副标题
+   */
+  subtitle: string;
+
+  /**
+   * 主图ID
+   */
+  mainPic: number;
+
+  /**
+   * 主图IDUrl
+   */
+  mainPicUrl: string;
+  /**
+   * 轮播图ID组
+   */
+  subPics: string;
+
+  /**
+   * 轮播图ID组Url
+   */
+  subPicsUrl: string;
+  /**
+   * 条形码
+   */
+  barCode: string;
+
+  /**
+   * 品牌ID
+   */
+  brandId: string | number;
+
+  /**
+   * 商品说明
+   */
+  content: string;
+
+  /**
+   * 最低价
+   */
+  minPrice: number;
+
+  /**
+   * 总销量
+   */
+  totalSales: number;
+
+  /**
+   * 可用库存
+   */
+  availableStock: number;
+
+  /**
+   * 综合评分
+   */
+  overallScore: number;
+
+  /**
+   * 排序
+   */
+  sortOrder: number;
+
+  /**
+   * 属性JSON
+   */
+  attrJson: string;
+
+  /**
+   * 状态
+   */
+  state: string;
+
+}
+
+export interface InfoForm extends BaseEntity {
+  /**
+   * ID
+   */
+  id?: string | number;
+
+  /**
+   * 乐观锁
+   */
+  version?: number;
+
+  /**
+   * 店铺ID
+   */
+  shopId?: string | number;
+
+  /**
+   * 分类ID
+   */
+  categoryId?: string | number;
+
+  /**
+   * 编码
+   */
+  spuCode?: string;
+
+  /**
+   * 名称
+   */
+  name?: string;
+
+  /**
+   * 副标题
+   */
+  subtitle?: string;
+
+  /**
+   * 主图ID
+   */
+  mainPic?: number;
+
+  /**
+   * 轮播图ID组
+   */
+  subPics?: string;
+
+  /**
+   * 条形码
+   */
+  barCode?: string;
+
+  /**
+   * 品牌ID
+   */
+  brandId?: string | number;
+
+  /**
+   * 商品说明
+   */
+  content?: string;
+
+  /**
+   * 最低价
+   */
+  minPrice?: number;
+
+  /**
+   * 总销量
+   */
+  totalSales?: number;
+
+  /**
+   * 可用库存
+   */
+  availableStock?: number;
+
+  /**
+   * 综合评分
+   */
+  overallScore?: number;
+
+  /**
+   * 排序
+   */
+  sortOrder?: number;
+
+  /**
+   * 属性JSON
+   */
+  attrJson?: string;
+
+  /**
+   * 状态
+   */
+  state?: string;
+
+}
+
+export interface InfoQuery extends PageQuery {
+
+  /**
+   * ID
+   */
+  id?: string | number;
+
+  /**
+   * 创建时间
+   */
+  createTime?: string;
+
+  /**
+   * 店铺ID
+   */
+  shopId?: string | number;
+
+  /**
+   * 分类ID
+   */
+  categoryId?: string | number;
+
+  /**
+   * 编码
+   */
+  spuCode?: string;
+
+  /**
+   * 名称
+   */
+  name?: string;
+
+  /**
+   * 条形码
+   */
+  barCode?: string;
+
+  /**
+   * 品牌ID
+   */
+  brandId?: string | number;
+
+  /**
+   * 状态
+   */
+  state?: string;
+
+    /**
+     * 日期范围参数
+     */
+    params?: any;
+}
+
+
+
